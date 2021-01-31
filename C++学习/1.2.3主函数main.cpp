cc2c8b077c99954f813520618b14570bfb118bfe
LOST <654725797@qq.com>
2021-01-31 11:04:57 +0800

    2021.1.31 11:04

---
+++ C++学习/1.2.3主函数main.cpp
@@ -0,0 +1,30 @@
+/*
+主函数main
+*/
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	/*
+//	
+//	C++主函数框架：
+//		int main()
+//		{
+//
+//			return 0;
+//		}
+//		应用程序入口，操作系统调用程序的接口，名称只能为main，不能修改，一个【项目】中只能有一个main函数
+//
+//	C与C++的不同：
+//		C的标准形式：int main(void){return 0;}
+//		C++的标准形式：int main(){return 0;}
+//		C/C++附带命令行参数的形式：int main(int argc , char* argv[]){return 0;}
+//	
+//	*/
+//
+//
+//	system("pause");
+//	return 0;
+//}

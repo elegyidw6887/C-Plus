56c235d6816714a7dba6560b9609ecec8287f15a
LOST <654725797@qq.com>
2021-01-31 10:19:42 +0800

    2021.1.31 10:19

---
+++ C++学习/1.2.1输出程序.cpp
@@ -0,0 +1,16 @@
+/*
+一个简单的C++输出程序
+*/
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//
+//	cout << "Hello World!" << endl;
+//	//C++使用cout来实现输出，形式如上
+//
+//	system("pause");
+//	return 0;
+//}

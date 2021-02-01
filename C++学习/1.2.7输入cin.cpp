c0c8d8617b54bcea7e0a26cf141d1697e281957f
LOST <654725797@qq.com>
2021-02-01 16:06:30 +0800

    2021.2.1 16:06

---
+++ C++学习/1.2.7输入cin.cpp
@@ -0,0 +1,25 @@
+/*
+C++输入：cin
+*/
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	/*
+//	
+//	cin:
+//		与cout一样，不是关键字也不是函数，而是对象
+//		在功能方面，与cout相同，但是比C语言的scanf更加的智能
+//	
+//	*/
+//	int a;
+//	char c;
+//	cin >> a >> c;
+//	cout << a << ' ' << c << endl;
+//
+//
+//	system("pause");
+//	return 0;
+//}

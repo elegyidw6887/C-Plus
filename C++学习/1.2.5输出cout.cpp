f9692216a99a035c67b365aeb7e71a00bd83d1e8
LOST <654725797@qq.com>
2021-02-01 15:37:33 +0800

    2021.2.1 15:37

---
+++ C++学习/1.2.5输出cout.cpp
@@ -0,0 +1,36 @@
+/*
+C++的输出函数：cout
+*/
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	/*
+//	
+//	C语言的输出与输入：
+//		printf与scanf
+//
+//	C++的输出与输入：
+//		cout与cin
+//			注：在C++中，cout与cin不是关键字也不是函数，而是一个【对象】，这点与C语言不同，在C语言中printf与scanf是关键词的同时也是函数
+//		cout的功能
+//			cout可以进行连续的输出，并且可以自动识别类型
+//	
+//	*/
+//	cout << "Hello World!" << ' ';
+//	cout << "Wow!" << 12;
+//	cout << '\n';
+//	//C语言的老式换行方式，C++中有新的方式
+//
+//	int a = 23;
+//	char c = 'H';
+//	double d = 123.123;
+//	cout << a << ' ' << c << ' ' << d;
+//	//如果要输出空格，不能和C语言一样，需要连续输出一个空格符号
+//
+//
+//	system("pause");
+//	return 0;
+//}

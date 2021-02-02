4022dfce3ec7599f0a28b79f445567e9aa5fdfd8
LOST <654725797@qq.com>
2021-02-02 12:37:00 +0800

    2021.2.2 12:36

---
+++ C++学习/1.5.4文件操作中预防头文件重复包含.cpp
@@ -0,0 +1,27 @@
+/*
+文件操作相关问题中预防头文件重复包含的方法
+*/
+
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	/*
+//	
+//	C++中添加的新的预防头文件重复包含的方法
+//			>#pragma once
+//			使用方法为直接添加在头文件上方
+//
+//	C语言中预防头文件重复包含的方法
+//			>#ifndef AAA
+//			>#define AAA
+//
+//			>#endif
+//	
+//	*/
+//
+//
+//	system("pause");
+//	return 0;
+//}
